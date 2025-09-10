a4b4990ace73f4c5920790e384ae040fee559a1a
Baumzin <brenosuvale@gmail.com>
2025-09-09 21:58:51 -0300

    Exexmplo realizado com sucesso!

---
+++ src/main/java/com/exemplo/api_produtos/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.exemplo.api_produtos.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.exemplo.api_produtos.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    List<Produto> findByName(String name);
+    List<Produto> findByNameContaining(String PartOfName);
+}
