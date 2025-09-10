f00c7c068f13f68edb5b1ebd0ee37368b6565276
BRENO VALENTIM BERNARDO <breno.bernardo01@etec.sp.gov.br>
2025-09-10 15:39:52 -0300

    Finalização sem teste do código

--- src/main/java/com/exemplo/api_produtos/controller/HabitController.java
+++ src/main/java/com/exemplo/api_produtos/controller/HabitController.java
@@ -11,11 +11,44 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.exemplo.api_produtos.model.Produto;
-import com.exemplo.api_produtos.repository.ProdutoRepository;
+import com.exemplo.api_produtos.model.Habit;
+import com.exemplo.api_produtos.repository.HabitRepository;
 
 @RestController
 @RequestMapping("/habits")
 public class HabitController {
-    // private final HabitRepository repository;
-} 
+    private final HabitRepository repository;
+
+    public HabitController(HabitRepository repository){
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public Habit create(@RequestBody Habit habit){
+        return repository.save(habit);
+    }
+
+    @GetMapping
+    public List<Habit> list() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Habit search(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Habit update(@PathVariable Long id, @RequestBody Habit newHabit) {
+        return repository.findById(id).map(habit -> {
+            habit.setName(newHabit.getName());
+            habit.setActive(newHabit.getActive());
+            return repository.save(habit);
+        }).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    } 
+}
--- src/main/java/com/exemplo/api_produtos/repository/HabitRepository.java
+++ src/main/java/com/exemplo/api_produtos/repository/HabitRepository.java
@@ -2,9 +2,9 @@ package com.exemplo.api_produtos.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.exemplo.api_produtos.model.Produto;
+import com.exemplo.api_produtos.model.Habit;
 
-public interface HabitRepository extends JpaRepository<Produto, Long> {
-    List<Produto> findByName(String name);
-    List<Produto> findByNameContaining(String PartOfName);
+public interface HabitRepository extends JpaRepository<Habit, Long> {
+    List<Habit> findByName(String name);
+    List<Habit> findByNameContaining(String PartOfName);
 }
